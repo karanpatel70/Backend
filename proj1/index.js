7d8820e65f30f915ed12478600325ab3dc33ea1d
karanPatel <karanpatel6898@gmail.com>
2026-07-28 22:04:05 +0530

    proj1

---
+++ proj1/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.get("/", (_req, res) => {
+  res.render("index");
+});
+
+app.get("/profile/:name", (req, res) => {
+  res.send(`Hello, ${req.params.name}!`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
